857a92668b4c5c53ff16008fd2fe1f59af49cb3f
Shola Ayeni <ayenisholah@yahoo.com>
2022-12-22 22:29:48 +0100

    basic ERC721 contract test

---
+++ test/unit/basicNft.test.js
@@ -0,0 +1,7 @@
+// We are going to skip a bit on these tests...
+
+const { assert } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+//writing the test code from here..
